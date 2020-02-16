47fb21e4c0bc0575483698bccf7f00b025d5ba63
Denis Putnam <denisputnam@verizon.net>
2020-02-16 10:24:04 -0500

    Created HungerGames

---
+++ HungerGames.sol
@@ -0,0 +1,76 @@
+pragma solidity >=0.4.22 <0.6.2;
+
+/// @title Problem 4.	Owned contract
+/// @author Denis M. Putnam
+/// @notice This contract establishes the owner and allows for an owner change.
+/// @dev Use at your own risk.
+contract Owned {
+    address payable owner;
+
+    /// @author Denis M. Putnam
+    /// @notice This modifier ensures that only the owner can call the funtion.
+    /// @dev No other details
+    modifier ownerOnly() {
+        require(msg.sender == owner,"Only the owner of the contract can execute this function.");
+        _;
+    }
+
+    /// @author Denis M. Putnam
+    /// @notice Constructor called when the contract is deployed.
+    /// @dev Sets the initial owner of the the contract.
+    constructor() public payable {
+        owner = msg.sender;
+    }
+
+    /// @author Denis M. Putnam
+    /// @notice Get the current owner.
+    /// @dev No other details.
+    /// @return address current owner.
+    function getCurrentOwner() public view returns(address payable) {
+        return owner;
+    }
+
+    /// @author Denis M. Putnam
+    /// @notice Change the current owner to the new owner.
+    /// @dev No other details.
+    /// @param newOwner new address payable owner.
+    function changeOwner(address payable newOwner) public ownerOnly() {
+        owner = newOwner;
+    }
+}
+
+contract HungerGames is Owned {
+
+    enum Gender { Male, Female }
+
+    /// @notice User struct of from, balance, and flag.
+    struct User {
+        string name;
+        uint256 age;
+        Gender gender;
+        bool flag;
+    }
+
+    /// @notice mapping of from addres to User.
+    mapping(string => User) users; // map name => User
+    mapping(uint256 => string) usersIndexMap; // index => name
+
+    function add(string memory name, uint256 age, uint256 gender) public {
+        users[name].name = name;
+        users[name].age = age;
+        users[name].gender = getGender(gender);
+        users[name].flag = true;
+    }
+
+    function getGender(uint256 index) public pure returns(Gender) {
+        require( index == 0 || index == 1, "argument must be 0 or 1");
+        return Gender(index);
+    }
+
+    function getPersonInfo(string memory name) public view returns (string memory, uint256, uint256) {
+        string memory _player = users[name].name;
+        uint256 _age = users[name].age;
+        uint256 _sex = uint256(users[name].gender);
+        return (_player, _age, _sex);
+    }
+}
